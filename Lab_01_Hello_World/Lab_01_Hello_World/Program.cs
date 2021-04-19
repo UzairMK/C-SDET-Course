dabc5014a63f0f5f36368ea65e299060991760ee
UzairMK <uzairmkhan584@gmail.com>
2021-04-19 16:44:36 +0100

    Add complete lab 01 material

---
+++ Lab_01_Hello_World/Lab_01_Hello_World/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_01_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // PascalCase -- used for Classes and Methods
+            // camelCase -- used for variables
+            // _camelCase -- used for private variables
+            // snake_case
+            // kabab-case
+
+            int x = 100;
+            x += 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                x += 1;
+            }
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
