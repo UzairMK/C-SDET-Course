d67d9728fce7b71a4e6d77a3cd51af8f667934aa
UzairMK <uzairmkhan584@gmail.com>
2021-04-20 12:08:19 +0100

    Add files from lab 2 (integer types) and lab 3 (operators)

---
+++ Lab_02_Integer_Types/Lab_02_Integer_Types/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab_02_Integer_Types
+{
+    class Program
+    {
+        //Integer overflow and underflow.
+        static void Main(string[] args)
+        {
+            uint a = Int32.MaxValue;
+            uint b = a + 1;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            float sum = 0;
+
+            for (int index = 0; index < 1000; index++)
+            {
+                sum += 1 / 3.0f;
+            }
+
+            Console.WriteLine("1/3 added 1000 times (" + sum + ") should be the same as 1/3 multiplied by 1000 (" + (1000/3.0f) + ")");
+
+            char c = 'c';
+            //Implicit casting
+            int d = c;
+            //Explicit casting
+            char e = (char)d;
+
+            Console.WriteLine($"char c ({c}) vs int c ({d})");
+
+            //No data loss because float is smaller than double
+            float f = 3.1415f;
+            double g = f;
+            //Cast has to be mentioned because double is more precise than float
+            float h = (float)g;
+
+            var convertExample = System.Convert.ToChar(1);
+            Console.WriteLine(convertExample);
+
+            int i = -255;
+            uint j = (uint)i;
+            Console.WriteLine($"when int -255 is explicitly converted to a uint, it give ({j})");
+
+            //Boolean
+            bool canFly = true;
+            int k = System.Convert.ToInt32(canFly);
+            Console.WriteLine($"bool ({canFly}) convert to int ({k})");
+
+            double pi = Math.PI;
+            var l = Math.Round(pi,2);
+            Console.WriteLine($"Rounding pi to 2 demical places ({l})");
+        }
+    }
+}
---
+++ Lab_03_Operators/Lab_03_Operators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_03_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, b = 1, c = 2;
+            b = a++;
+            c = ++a;
+
+            var sum = a + b - c;
+
+            var years = 11;
+            var dogYears = years * 7;
+
+            var pounds = 15;
+            var kilos = pounds / 2.2;
+
+            var d = 5.0 / 2.0;
+            var e = 5 / 2;
+            var f = 5.0 / 2;
+
+            //What is 26 days in weeks and days
+            var days = 26;
+            Console.WriteLine($"There is/are {days/7} week(s) and {days%7} day(s) in {days} day(s)");
+            
+            //Bitwise operators
+            Console.WriteLine($"5 & 1 = {5 & 1}");
+            Console.WriteLine($"5 | 1 = {5 | 1}");
+            Console.WriteLine($"5 ^ 1 = {5 ^ 1}");
+
+            Console.WriteLine($"Left shift: 9 << 3 = {9 << 3}");
+            Console.WriteLine($"Right shift: 9 >> 3 = {9 >> 3}");
+
+        }
+    }
+}
